f97076ad86771925f84b7d6fb51d146019262a0c
Alex Barber <alexanderbdev@gmail.com>
2019-06-26 13:51:19 -0500

    api

--- client/components/Map.js
+++ client/components/Map.js
@@ -5,7 +5,7 @@ import {GoogleMap, Marker, withGoogleMap, withScriptjs} from 'react-google-maps'
 const MyMapComponent = compose(
   withProps({
     googleMapURL:
-      'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places',
+      'https://maps.googleapis.com/maps/api/js?key=AIzaSyCRFUgZ3AOmMoeVXjyLVS44WU3uxS5P7YI&js?v=3.exp&libraries=geometry,drawing,places',
     loadingElement: <div style={{height: '100%'}} />,
     containerElement: <div style={{height: `400px`}} />,
     mapElement: <div style={{height: `100%`}} />
@@ -14,7 +14,9 @@ const MyMapComponent = compose(
   withGoogleMap
 )(props => (
   <GoogleMap defaultZoom={8} defaultCenter={{lat: 41.8781, lng: -87.6298}}>
-    {props.isMarkerShown && <Marker position={{lat: 41.8781, lng: -87.6298}} />}
+    {props.isMarkerShown && (
+      <Marker position={{lat: 41.913501, lng: -87.648163}} />
+    )}
   </GoogleMap>
 ))
 
