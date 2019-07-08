d99aa7fb034c8b4f2b18641240cf71cb6c244dc4
Alex Barber <alexanderbdev@gmail.com>
2019-07-08 13:35:53 -0500

    fixing app layout & screen realestate

--- client/components/NavBar/AppBar.js
+++ client/components/NavBar/AppBar.js
@@ -5,6 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import ProfileMenu from './LoginMenu'
 import NavMenu from './NavMenu'
+import Switch from '@material-ui/core/Switch'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -23,6 +24,7 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             BeatenPath
           </Typography>
+          <Switch />
           {/*<ProfileMenu />*/}
         </Toolbar>
       </AppBar>
