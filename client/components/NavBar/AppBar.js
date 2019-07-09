730f817a2192a0d4f0078c7dedaac87a5f7d6dbc
Michelle Kiss <picturethis56@gmail.com>
2019-07-09 14:44:04 -0500

    toggle buttons in nav working

--- client/components/NavBar/AppBar.js
+++ client/components/NavBar/AppBar.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
+import {StoreContext} from '../../app'
 import {makeStyles} from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import ProfileMenu from './LoginMenu'
 import Button from '@material-ui/core/Button'
+import ViewModuleIcon from '@material-ui/icons/ViewModule'
 import NavMenu from './NavMenu'
 import Switch from '@material-ui/core/Switch'
+import {defaultProps} from 'recompose'
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
   },
   title: {
     flexGrow: 1
@@ -20,12 +24,28 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-export default function ButtonAppBar() {
-  const classes = useStyles()
+export const ButtonAppBar = () => {
+  const [state, dispatch] = useContext(StoreContext)
 
   return (
+    <ButtonAppBarView
+      segments={state.segments}
+      dispatch={dispatch}
+      placePreview={state.placePreview}
+      places={state.places}
+      journeys={state.journeys}
+      journey={state.journey}
+      mode={state.mode}
+    />
+  )
+}
+
+export function ButtonAppBarView(props) {
+  const classes = useStyles()
+  console.log('mode: ', props.mode)
+  return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="static" classes={{root: classes.root}}>
         <Toolbar variant="dense">
           {/*<NavMenu />*/}
 
@@ -33,27 +53,42 @@ export default function ButtonAppBar() {
             {<img src="/BeatenPath.png" height="25px" />}
           </Typography>
           <div style={{marginBottom: '1vh'}}>
-            <Button
-              variant="contained"
-              size="small"
-              color="primary"
-              className={classes.margin}
-            >
-              Create
-            </Button>
+            {(props.mode === 'find' || props.mode === 'viewOnly') && (
+              <Button
+                variant="contained"
+                size="small"
+                color="primary"
+                className={classes.margin}
+                onClick={function() {
+                  props.dispatch({type: 'CHANGE_MODE', mode: 'create'})
+                  props.dispatch({type: 'CLEAR_PLACES'})
+                  props.dispatch({type: 'SET_SINGLE_JOURNEY', journey: {}})
+                }}
+              >
+                Create
+              </Button>
+            )}
 
-            <Button
-              variant="contained"
-              size="small"
-              color="primary"
-              className={classes.margin}
-            >
-              Find
-            </Button>
+            {(props.mode === 'create' || props.mode === 'viewOnly') && (
+              <Button
+                variant="contained"
+                size="small"
+                color="primary"
+                className={classes.margin}
+                onClick={function() {
+                  props.dispatch({type: 'CHANGE_MODE', mode: 'find'})
+                }}
+              >
+                Find
+              </Button>
+            )}
           </div>
+          <ViewModuleIcon />
           <ProfileMenu />
         </Toolbar>
       </AppBar>
     </div>
   )
 }
+
+export default ButtonAppBar
