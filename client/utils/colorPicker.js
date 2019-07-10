c85f7c0be54b64d2efc92032bb8262bca2426665
Michelle Kiss <picturethis56@gmail.com>
2019-07-10 16:11:37 -0500

    new colors added

--- client/utils/colorPicker.js
+++ client/utils/colorPicker.js
@@ -1,14 +1,30 @@
 export const colorPicker = index => {
   switch (index) {
     case -1:
-      return '#E1442C'
+      return '#f9a752' //light orange
     case 0:
-      return '#31ACC4'
+      return '#75aa8e' // light turquoise
     case 1:
-      return '#AC31C4'
+      return '#f9c266' // light gold
     case 2:
-      return '#49C431'
+      return '#e68762' // pink
     case 3:
-      return '#E1982C'
+      return '#6b7086' // light teal blue
+    case 4:
+      return '#f9d45a' //light yellow
+    case 5:
+      return '#66c7df' // light blue
+    case 6:
+      return '#f9a752' //light orange
+    case 7:
+      return '#a3c075' // light yellow green
+    case 8:
+      return '#928476' // light taupe
+    case 9:
+      return '#856cb5' // light purple
+    case 10:
+      return '#c74742' // rust
+    case 11:
+      return '#ac7566' //brown
   }
 }
